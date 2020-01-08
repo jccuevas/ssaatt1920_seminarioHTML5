dae1ac03b2eb95c97a609b707a7916727bbd9740
jccuevas <jccuevas@ujaen.es>
2020-01-08 19:50:05 +0100

    Añadido multimedia

--- form.php
+++ form.php
@@ -1,25 +1,33 @@
-<?php
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+    <head>
+        <meta charset="utf-8">
+        <title>Ejemplos seminario HTML5</title>
+    </head>
+    <body> 
+        <?php
+        
+        if ($_GET == null) {
+            echo "No hay parámetros por GET" . "<br>";
+        } else {
+            $valor = "";
+            $clave = "";
+            foreach ($_GET as $clave => $valor) {
+                echo "Parámetro GET: " . $clave . " valor=" . $valor . "<br>";
+            }
+        }
 
-if ($_GET == null) {
-    echo "No hay parámetros por GET" . "<br>";
-
-} else {
-    $valor = "";
-    $clave = "";
-    foreach ($_GET as $clave => $valor) {
-        echo "Parámetro GET " . $clave . " valor=" . $valor . "<br>";
-    }
-}
-
-if ($_POST == null) {
-    echo "No hay parámetros por POST" . "<br>";
-
-} else {
-    $valor = "";
-    $clave = "";
-    foreach ($_POST as $clave => $valor) {
-        echo "Parámetro POST" . $clave . " valor=" . $valor . "<br>";
-    }
-}
+        if ($_POST == null) {
+            echo "No hay parámetros por POST" . "<br>";
+        } else {
+            $valor = "";
+            $clave = "";
+            foreach ($_POST as $clave => $valor) {
+                echo "Parámetro POST: " . $clave . " valor=" . $valor . "<br>";
+            }
+        }
+        ?>
+    </body> 
+</html> 
 
 
